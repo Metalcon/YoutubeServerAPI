8a54c2ec75575f3bdc29db38e8ab592aea7eed7b
Christian Schowalter <cschowalter@uni-koblenz.de>
2014-03-20 15:21:50 +0100

    finished bulk request (also fixed an exception which occurs when a result is null)

--- src/main/java/freebaseclient/BulkReconciliationMetalcon.java
+++ src/main/java/freebaseclient/BulkReconciliationMetalcon.java
@@ -8,12 +8,14 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Properties;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.omg.CORBA.RepositoryIdHelper;
 
 import com.google.api.client.http.ByteArrayContent;
 import com.google.api.client.http.GenericUrl;
@@ -26,155 +28,170 @@ import com.jayway.jsonpath.JsonPath;
 
 public class BulkReconciliationMetalcon {
 
-	//TODO: find a good value for maximum number of requests per query
-	private static final int maximalQueryLength = 100;
+	// TODO: find a good value for maximum number of requests per query
+	private static final int maximalQueryLength = 10;
 	public static Properties properties = new Properties();
 	public static String outputString = null;
 
 	public static void main(String[] args) throws IOException, ParseException {
-		
-		
 
-			try {
-				properties.load(new FileInputStream("freebase.properties"));
-			} catch (IOException e1) {
-				System.out.println("Problem reading properties!");
-				e1.printStackTrace();
+		try {
+			properties.load(new FileInputStream("freebase.properties"));
+		} catch (IOException e1) {
+			System.out.println("Problem reading properties!");
+			e1.printStackTrace();
+		}
+
+		JSONParser parser = new JSONParser();
+
+		// GenericUrl url = new
+		// GenericUrl("https://www.googleapis.com/freebase/v1/reconcile");
+
+		List<String> bandListArray = new ArrayList<String>();
+		String line;
+		try {
+			FileReader fileReader = new FileReader("Band.csv");
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			for (int i = 0; (line = bufferedReader.readLine()) != null; ++i) {
+				bandListArray.add(line);
 			}
+		} catch (IOException e) {
+			System.err.println("Problem reading Bandlist!");
+			e.printStackTrace();
+		}
 
-			JSONParser parser = new JSONParser();
-			
-			// GenericUrl url = new
-			// GenericUrl("https://www.googleapis.com/freebase/v1/reconcile");
-
-			
-
-			ArrayList<String> bandListArray = new ArrayList<String>();
-			String line;
-			try {
-				FileReader fileReader = new FileReader("Band_LITE.csv");
-				BufferedReader bufferedReader = new BufferedReader(fileReader);
-
-				for (int i = 0; (line = bufferedReader.readLine()) != null; ++i) {
-					bandListArray.add(line);
-					// System.out.println(line + " " + i);
-				}
-			} catch (IOException e) {
-				System.out.println("Problem reading Bandlist!");
-				e.printStackTrace();
-			}
-			
-			if (bandListArray.size() > maximalQueryLength){
-				//TODO: implement partitioned request
-				ArrayList<ArrayList> partitionedListsContainer = new ArrayList<ArrayList>();
-				for (int i = 0; i < bandListArray.size(); i+=maximalQueryLength) {
-					
-					if (i+maximalQueryLength-1 < bandListArray.size()){
-					ArrayList<String> bandListPart = (ArrayList<String>) bandListArray.subList(i, i+maximalQueryLength-1);
-					HttpResponse httpResponse = BuildRequest(bandListPart).execute();
-					outputString += parseResponse (httpResponse , bandListPart);
+		if (bandListArray.size() > maximalQueryLength) {
+			for (int i = 0; i < bandListArray.size(); i += maximalQueryLength - 1) {
+				if (i + maximalQueryLength - 1 < bandListArray.size()) {
+					List<String> bandListPart = bandListArray.subList(i, i
+							+ maximalQueryLength - 1);
+					//System.out.println("about to send request number" + i
+					//		+ " to " + (i + maximalQueryLength - 1));
+					HttpResponse httpResponse = BuildRequest(bandListPart)
+							.execute();
+					//System.out.println("request sent. About to parse response");
+					// System.out.println("response to parse --> " +
+					// httpResponse.parseAsString());
+					outputString += parseResponse(httpResponse, bandListPart);
 				} else {
-					ArrayList<String> bandListPart = (ArrayList<String>) bandListArray.subList(i, bandListArray.size()-1);
-					HttpResponse httpResponse = BuildRequest(bandListPart).execute();
-					outputString += parseResponse (httpResponse , bandListPart);
+					List<String> bandListPart = bandListArray.subList(i,
+							bandListArray.size());
+					//System.out.println("about to send request number" + i
+					//		+ " to " + (bandListArray.size()));
+					HttpResponse httpResponse = BuildRequest(bandListPart)
+							.execute();
+					//System.out.println("request sent. About to parse response");
+					// System.out.println("response to parse --> " +
+					// httpResponse.parseAsString());
+					outputString += parseResponse(httpResponse, bandListPart);
 				}
-				
-			}}
-			else{
+
+			}
+		} else {
 			HttpResponse httpResponse = BuildRequest(bandListArray).execute();
-			outputString = parseResponse (httpResponse , bandListArray);
+			outputString = parseResponse(httpResponse, bandListArray);
+		}
+		writeToFile(outputString);
 	}
-			writeToFile(outputString);
-			}
 
-			// Freebase queries have a limit concerning the number of requested
-			// entries per query so we have to divide large lists to severeal
-			// requests.
-			// TODO: implement request splitting
+	// Freebase queries have a limit concerning the number of requested
+	// entries per query so we have to divide large lists to severeal
+	// requests.
+	// TODO: implement request splitting
 
-			public static HttpRequest BuildRequest(ArrayList<String> bandListArray) throws IOException{
-				
-				GenericUrl url = new GenericUrl("https://www.googleapis.com/rpc");
-				JSONArray requestBody = new JSONArray();
-				HttpTransport httpTransport = new NetHttpTransport();
-				HttpRequestFactory requestFactory = httpTransport
-						.createRequestFactory();
-				
-			for (int i = 0; i < bandListArray.size(); i++) {
+	public static HttpRequest BuildRequest(List<String> bandListArray)
+			throws IOException {
 
-				String bandDataColumn = bandListArray.get(i);
-				String[] bandDataSplitArray = bandDataColumn.split("\t");
-				System.out.println(bandDataSplitArray[1]);
+		GenericUrl url = new GenericUrl("https://www.googleapis.com/rpc");
+		JSONArray requestBody = new JSONArray();
+		HttpTransport httpTransport = new NetHttpTransport();
+		HttpRequestFactory requestFactory = httpTransport
+				.createRequestFactory();
 
-				JSONObject requestBodyContent = new JSONObject();
-				// prepare request metadata
-				requestBodyContent.put("jsonrpc", "2.0");
+		for (int i = 0; i < bandListArray.size(); i++) {
 
-				requestBodyContent.put("id", i);
-				requestBodyContent.put("method", "freebase.reconcile");
-				requestBodyContent.put("apiVersion", "v1");
+			String bandDataColumn = bandListArray.get(i);
+			String[] bandDataSplitArray = bandDataColumn.split("\t");
+			System.out.println(bandDataSplitArray[1]);
 
-				JSONObject exampleRequestContent = new JSONObject();
-				exampleRequestContent.put("name", bandDataSplitArray[1]);
+			JSONObject requestBodyContent = new JSONObject();
+			// prepare request metadata
+			requestBodyContent.put("jsonrpc", "2.0");
 
-				JSONArray exampleRequestContentKind = new JSONArray();
-				exampleRequestContentKind.add("/music/artist");
-				exampleRequestContent.put("type", exampleRequestContentKind);
+			requestBodyContent.put("id", i);
+			requestBodyContent.put("method", "freebase.reconcile");
+			requestBodyContent.put("apiVersion", "v1");
 
-				JSONArray exampleRequestEmpty = new JSONArray();
+			JSONObject exampleRequestContent = new JSONObject();
+			exampleRequestContent.put("name", bandDataSplitArray[1]);
 
-				// to request certain kinds of content, simply write it as a key
-				// with null Value
-				// exampleRequestContent.put("/music/album",
-				// exampleRequestEmpty);
-				JSONArray exampleRequestContentProp = new JSONArray();
-				exampleRequestContentProp
-						.add("/music/artist/genre:Heavy metal");
-				exampleRequestContent.put("prop", exampleRequestContentProp);
+			JSONArray exampleRequestContentKind = new JSONArray();
+			exampleRequestContentKind.add("/music/artist");
+			exampleRequestContent.put("type", exampleRequestContentKind);
 
-				// API-Key is needed for every single concept... wtf?!
-				exampleRequestContent.put("key", properties.get("API_KEY"));
+			JSONArray exampleRequestEmpty = new JSONArray();
 
-				requestBodyContent.put("params", exampleRequestContent);
+			// to request certain kinds of content, simply write it as a key
+			// with null Value
+			// exampleRequestContent.put("/music/album",
+			// exampleRequestEmpty);
+			JSONArray exampleRequestContentProp = new JSONArray();
+			exampleRequestContentProp.add("/music/artist/genre:Heavy metal");
+			exampleRequestContent.put("prop", exampleRequestContentProp);
 
-				requestBody.add(requestBodyContent);
-			}
-			String requestBodyString = requestBody.toString();
-			System.out.println(requestBodyString);
+			// API-Key is needed for every single concept... wtf?!
+			exampleRequestContent.put("key", properties.get("API_KEY"));
 
-			HttpRequest request = requestFactory.buildPostRequest(url,
-					ByteArrayContent.fromString("application/json",
-							requestBodyString));
+			requestBodyContent.put("params", exampleRequestContent);
 
-			return request;
-			}
-			
-			
-			
-			public static String parseResponse(HttpResponse httpResponse , ArrayList bandListArray) throws ParseException, IOException{
+			requestBody.add(requestBodyContent);
+		}
+		String requestBodyString = requestBody.toString();
+		//System.out.println(requestBodyString);
 
-			JSONArray response = new JSONArray();
-			JSONParser jsonparser = new JSONParser();
+		HttpRequest request = requestFactory.buildPostRequest(url,
+				ByteArrayContent.fromString("application/json",
+						requestBodyString));
+
+		return request;
+	}
+
+	public static String parseResponse(HttpResponse httpResponse,
+			List bandListArray) throws ParseException, IOException {
+
+		JSONArray response = new JSONArray();
+		JSONParser jsonparser = new JSONParser();
+		try {
 			response = (JSONArray) jsonparser.parse(httpResponse
 					.parseAsString());
-
-
-
-			String responseString = "";
-
-			for (int j = 0; j < response.size(); j++) {
-				JSONObject responseEntry = (JSONObject) jsonparser
-						.parse(response.get(j).toString());
-				JSONObject responseEntryResult = (JSONObject) responseEntry
-						.get("result");
-				JSONArray responseEntryResultCandidates = (JSONArray) responseEntryResult
-						.get("candidate");
+			System.out.println(response.toString());
+		} catch (ClassCastException ce) {
+			System.err
+					.println("Typecast failed. Response is probably broken. Can be caused by bad request");
+		}
+		String responseString = "";
+		boolean candidateFound;
+		for (int j = 0; j < response.size(); j++) {
+			candidateFound = false;
+			JSONObject responseEntry = (JSONObject) jsonparser.parse(response
+					.get(j).toString());
+			JSONObject responseEntryResult = (JSONObject) responseEntry
+					.get("result");
+			JSONArray responseEntryResultCandidates = (JSONArray) responseEntryResult
+					.get("candidate");
+			if (responseEntryResultCandidates == null) {
+				responseString += bandListArray.get(j) + "\t" + "NO_MID_FOUND"
+						+ "\t" + "0.0" + "\n";
+				System.out.println("null result received -->"
+						+ bandListArray.get(j));
+			} else {
 
 				/*
 				 * a single entry can have multiple candidates It seems like the
 				 * candidates are sorted by their confidence score TODO: look up
 				 * whether this is reliable
 				 */
+
 				for (Object candidate : responseEntryResultCandidates) {
 					responseString += (bandListArray.get(j)
 							+ "\t"
@@ -182,27 +199,26 @@ public class BulkReconciliationMetalcon {
 							+ "\t"
 							+ JsonPath.read(candidate, "$.confidence")
 									.toString() + "\n");
-
 					// remove this break if you want to get more than one
 					// result!
 					break;
 				}
-
 			}
-			System.out.println(responseString);
-			return(responseString);
+		}
 
+		System.out.println(responseString);
+		return (responseString);
 
-		} 
-			
-			public static void writeToFile(String dataToSave) throws IOException{
-				File outputFile = new File("Band_Freebase_matched.csv");
-				if (!outputFile.exists()) {
-					outputFile.createNewFile();
-				}
-				FileWriter fileWriter = new FileWriter(outputFile.getAbsoluteFile());
-				BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-				bufferedWriter.write(dataToSave);
-				bufferedWriter.close();
-			}
 	}
+
+	public static void writeToFile(String dataToSave) throws IOException {
+		File outputFile = new File("Band_Freebase_matched.csv");
+		if (!outputFile.exists()) {
+			outputFile.createNewFile();
+		}
+		FileWriter fileWriter = new FileWriter(outputFile.getAbsoluteFile());
+		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+		bufferedWriter.write(dataToSave);
+		bufferedWriter.close();
+	}
+}
