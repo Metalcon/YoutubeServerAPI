244de9795792988a2fe2b9ded748b5d89715be2d
Christian Schowalter <cschowalter@uni-koblenz.de>
2014-03-20 16:15:48 +0100

    started AlbumSearch implementation

---
+++ src/main/java/freebaseclient/BulkAlbumSearch.java
@@ -0,0 +1,15 @@
+package freebaseclient;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+/**
+ * @author Christian Schowalter
+ */
+public class BulkAlbumSearch {
+	
+	public static void main(String[] args){
+		//TODO: implement album search, similar to reconciliation
+	}
+}
