ed739130537e2ac177f821f12bd2d92bac7e0f6f
Christian Schowalter <cschowalter@uni-koblenz.de>
2014-04-01 14:33:28 +0200

    fixed request string

--- src/main/java/freebaseclient/SingleAlbumSearch.java
+++ src/main/java/freebaseclient/SingleAlbumSearch.java
@@ -1,14 +1,6 @@
 package freebaseclient;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Properties;
 
 import org.json.simple.JSONArray;
@@ -36,7 +28,7 @@ public class SingleAlbumSearch {
 					"https://www.googleapis.com/freebase/v1/mqlread");
 
 			// TODO: implement reading mid from file
-			String bandMid = "/m/014_xj\"";
+			String bandMid = "/m/02g_zp";
 
 			String query = "[{\"mid\":\""
 					+ bandMid
@@ -62,33 +54,5 @@ public class SingleAlbumSearch {
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
-	}
-
-	private static List<String> readInputFile(String inputFile) {
-		String line;
-		List<String> listArray = new ArrayList<String>();
-		try {
-			FileReader fileReader = new FileReader(inputFile + ".csv");
-			BufferedReader bufferedReader = new BufferedReader(fileReader);
-			for (int i = 0; (line = bufferedReader.readLine()) != null; ++i) {
-				listArray.add(line);
-			}
-		} catch (IOException e) {
-			System.err.println("Problem reading List!");
-			e.printStackTrace();
-		}
-		return listArray;
-	}
-
-	public static void writeToFile(String dataToSave, String saveFileName)
-			throws IOException {
-		File outputFile = new File(saveFileName + ".csv");
-		if (!outputFile.exists()) {
-			outputFile.createNewFile();
-		}
-		FileWriter fileWriter = new FileWriter(outputFile.getAbsoluteFile());
-		BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
-		bufferedWriter.write(dataToSave);
-		bufferedWriter.close();
 	}
 }
