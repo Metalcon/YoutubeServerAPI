91f88dbb65169c1ca9dbb5b3513cb41760b48bfb
Christian Schowalter <cschowalter@uni-koblenz.de>
2014-04-02 13:59:22 +0200

    added getters and setters to FreebaseMetaData class

--- src/main/java/api/FreebaseMetaData.java
+++ src/main/java/api/FreebaseMetaData.java
@@ -13,4 +13,29 @@ public class FreebaseMetaData {
 	private String mid;
 	private double confidence;
 	private String type;
+
+	public String getMid() {
+		return mid;
+	}
+
+	public void setMid(String mid) {
+		this.mid = mid;
+	}
+
+	public double getConfidence() {
+		return confidence;
+	}
+
+	public void setConfidence(double confidence) {
+		this.confidence = confidence;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
 }
